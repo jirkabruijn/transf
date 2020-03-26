0bfc276b3b220dbb7020468c45aaea245466c3a2
Jirka Bruijn <jirka.bruijn@gmail.com>
2020-03-26 18:02:11 +0100

    Playing with matrices

--- transf/transf.py
+++ transf/transf.py
@@ -5,8 +5,12 @@ import numpy as np
 
 class Transformation:
 
+    def __init__(self):
+        self.mx = np.array([[1,0,0],[0,1,0],[0,0,1]])
+
     def compose(self, f, g):
         func = lambda x: f(g(x))
+        func.mx = lambda x: self.mx * f(x) * g(x)
         return func
 
     def __matmul__(self, *functions):
@@ -16,29 +20,43 @@ class Transformation:
         #print("composed:", composed)
         return composed
 
-
 class Scaling(Transformation):
     def __init__(self, scale):
         Transformation.__init__(self)
         self.scale = scale
+        self.mx = []
 
     def __call__(self, p):
         x, y = p
-        return [x*self.scale, y*self.scale]
+        result = [x*self.scale, y*self.scale]
+        self.mx = np.array(result)
+        return result
+
+    @property
+    def matrix(self):
+        return self.mx
 
 class Translation(Transformation):
     def __init__(self, shift):
         Transformation.__init__(self)
         self.shift = shift
+        self.mx = []
 
     def __call__(self, p):
-        return [x + y for x, y in zip(p, self.shift)]
+        result = [x + y for x, y in zip(p, self.shift)]
+        self.mx = np.array(result)
+        return result
+
+    @property
+    def matrix(self):
+        return self.mx
 
 
 class Rotation(Transformation):
     def __init__(self, angle):
         Transformation.__init__(self)
         self.angle = angle
+        self.mx = []
 
     def __call__(self, p):
         return self.rotate(p,[0,0],self.angle)
@@ -49,4 +67,9 @@ class Rotation(Transformation):
 
         qx = ox + math.cos(-angle) * (px - ox) - math.sin(-angle) * (py - oy)
         qy = oy + math.sin(-angle) * (px - ox) + math.cos(-angle) * (py - oy)
+        self.mx = np.array([qx, qy])
         return qx, qy
+
+    @property
+    def matrix(self):
+        return self.mx
--- transformations_test.py
+++ transformations_test.py
@@ -27,11 +27,45 @@ composed = scaling @ translation
 assert np.allclose(composed(p), scaling(translation(p)))
 # A transformation has an attribute `matrix` that returns the corresponding affine
 # matrix of shape `[3, 3]`
+a = np.array([1,2])
+b = np.array([[3],[4]])
+c = a @ b
+print("a:", a)
+print("b:",b)
+print("c:",c)
+print("translation.matrix")
+print(translation.matrix)
+print("scaling.matrix")
+print(scaling.matrix)
+print(scaling.matrix.T)
+print("scaling.matrix @ translation.matrix")
+print(scaling.matrix * translation.matrix)
+test = translation.matrix.reshape((2,1))
+print("test")
+print(test)
+print("scaling.matrix @ test")
+print(scaling.matrix * test)
+print("na")
+na = np.array([[3., 0., 3.],
+[0., 3., 6.],
+[0., 0., 1.]])
+print(na)
+
+#print("composed.matrix")
+#print(composed.matrix)
+
+
+
+
+
+exit()
+
 assert np.allclose(composed.matrix,
 np.array([[3., 0., 3.],
 [0., 3., 6.],
 [0., 0., 1.]])
 )
+
 # However the `matrix` attribute is not writeable:
 try:
     composed.matrix = np.eye(3)
