9278c63e7641eb497d88ed91be3b037fb0591596
Jirka Bruijn <jirka.bruijn@gmail.com>
2020-03-27 11:02:25 +0100

    Added basic setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+exec(open('transf/version.py').read())
+
+REQUIRES = ['numpy',
+            'math',
+            'functools']
+setup(
+    name = 'transf',
+    version=__version__,
+    description = 'A test concerning transformations',
+    url = 'https://github.com/jirkabruijn/transf.git',
+    author = 'Jiri Bruijn',
+    author_email = 'jirka.bruijn@gmail.com',
+    license = 'unlicensed',
+    packages=['transf'],
+    zip_safe=False,
+    install_requires=REQUIRES
+)
